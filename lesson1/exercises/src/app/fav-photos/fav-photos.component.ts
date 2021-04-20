888ac100ad5a2d69881372f8c06ff50683e3f0b0
Prerna Badhwar <prernabadhwar@gmail.com>
2021-04-19 19:45:23 -0500

    Completed Excercise Part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images-na.ssl-images-amazon.com/images/I/81v7Qy10MPL._AC_SL1500_.jpg';
+  image3 = 'assets/images/USA.jpg';
 
   constructor() { }
 
